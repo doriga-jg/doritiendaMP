5df1110564bcab0de7fa4dcd4e8af6714bce26aa
Jorge Gabriel Pérez Bobadilla <jperezb@corporativojp.com.mx>
2021-01-13 00:28:42 -0600

    :construction: Test1 for browserSync bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,99 @@
-var gulp       = require('gulp'),
-    gutil      = require('gulp-util'),
-    rename     = require('gulp-rename'),
-    uglify     = require('gulp-uglify'),
-    changed    = require('gulp-changed'),
-    browserSync= require('browser-sync').create(),
-    reload     = browserSync.reload,
-    autoprefix = require('gulp-autoprefixer'),
-    sourcemaps = require('gulp-sourcemaps'),
-    concat     = require('gulp-concat'),
-    sass       = require('gulp-sass')
-;
+var gulp        = require('gulp');
+const minifyCss = require('gulp-clean-css');
+//var coffe          = require('gulp-coffee');
+var sass        = require('gulp-sass');
+var notify      = require('gulp-notify');
+var browserSync = require('browser-sync');
+const { parallel } = require('gulp');
+var reload      = browserSync.reload;
 
-var watch = {
-    js : 'public/dev_assets/js/**/*.js',
-    sass   : 'public/dev_assets/scss/**/*.scss',
-    php    : 'app/Views/**/*.php'
+var paths = {
+    php:'app/Views/**/*.php',
+    css:'app/public/dev_assets/scss/**/*.scss',
+    script:'app/public/dev_assets/js/**/*.js'
 };
 
-var src  = {
-    js : 'public/dev_assets/js/**/*.js',
-    sass   : 'public/dev_assets/scss/tienda.scss',
-    php    : 'app/Views/**/*.php'
-};
+// css task
 
-var compiled = {
-    js : 'public/dev_assets/js/',
-    sass   : 'public/dev_assets/css/'
-};
+gulp.task('mincss', function(){
 
-var distribution = {
-    js  : './distribution/src/',
-    css : './distribution/src/',
-    php : './distribution/'
-}
+    return gulp.src(paths.css)
 
-function browser_sync(done){
-    browserSync.init({
-        proxy:"localhost/",
-        port: 8080
-    });
-    done();
-}
+        .pipe(sass().on('error', sass.logError))
 
-gulp.task('sass', function(done){
-    gulp.src(src.sass)
-        .pipe(sourcemaps.init())
-        .pipe(
-            sass({
-                errorLogToConsole : true,
-                outputStyle   : 'compressed'
-            })
-        )
-        .on('error', console.error.bind( console ) )
-        .pipe(
-            autoprefix({
-                browsers: ['last 2 versions'],
-                cascade: false
-            })
-        )
-        .pipe( rename( {suffix:'.min'} ) )
-        .pipe( sourcemaps.write('./') )
-        .pipe( gulp.dest( compiled.sass) )
-        .pipe( browserSync.stream());
-    done();
-})
+        .pipe(minifyCss())
 
-gulp.task('js', function(done) {
-    gulp.src(src.js)
-        .pipe(sourcemaps.init())
-        .pipe(concat('app.js'))
-        .pipe(
-            js( { bare: true } )
-                .on('error', gutil.log)
-        )
-        .pipe( gulp.dest(compiled.js) )
-        .pipe( browserSync.stream() );
-    done();
+        .pipe(gulp.dest('main'))
+
+        .pipe(reload({stream:true}));
+
 });
 
-gulp.task('php', function(done) {
-    gulp.src(src.php)
-        .pipe(browserSync.stream())
-        .pipe(reload());
+// ////////////////////////////////////////////////
+// HTML task
+// ///////////////////////////////////////////////
+
+gulp.task('php', function(){
+    gulp.src(paths.php)
+    .pipe(reload({stream:true}));
+});
+
+
+// Javascrit task
+
+gulp.task('scripts', function(){
+
+    return gulp.src(paths.script)
+
+        .pipe(coffee())
+
+        .pipe(gulp.dest('js'))
+
+        .pipe(reload({stream:true}));
+
+});
+
+// ////////////////////////////////////////////////
+// Browser-Sync Tasks
+// // /////////////////////////////////////////////
+
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        },
+        port: 8080,
+        open: true,
+        notify: false
+    });
+});
+
+/*gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }, 
+        port: 8080,
+        open:true,
+        notify: false
+    }); 
+});*/
+
+// BrowserSync Reload
+/*function browserSyncReload(done) {
+    browsersync.reload();
     done();
-})
+}*/
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function watch_files(){
-    gulp.watch(watch.sass   ,gulp.series('sass'));
-    gulp.watch(watch.js ,gulp.series('js'));
-    gulp.watch(watch.php, gulp.series('php'));
-}
+gulp.task('watcher',function(){
 
-gulp.task("start", gulp.parallel(watch_files, browser_sync));
+    gulp.watch(paths.css, gulp.series('mincss'));
+    gulp.watch(paths.script, gulp.series('scripts'));
+    gulp.watch(paths.php, gulp.series('php'/*, browserSyncReload*/));
+
+});
+
+gulp.task('default', gulp.parallel('browserSync', 'watcher'));
